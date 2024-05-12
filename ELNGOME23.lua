d590e0ac4fc0c698ccdc0310707bddbe299f1e7c
Mahmoud Basem <35140942+mahmoudbasem@users.noreply.github.com>
2024-05-13 01:07:52 +0300

    Updatedg ELNGOME23.lua

--- ELNGOME23.lua
+++ ELNGOME23.lua
@@ -1,6 +1,6 @@
 local current_script = gg.getFile():match("[^/]+$")
 if current_script == 'ELNGOME__23.lua' then
-local url = 'https://bit.ly/3JNXx86'
+local url = 'https://bit.ly/3UXiQKQ'
 local new_script = 'Stars Team.lua'
 	gg.alert('⛔ لقد انتهي هذا الملف سيتم تحميل الملف الجديد وحذف هذا الملف ⛔\n🆕 اسم الملف الجديد Stars Team.lua 🆕')
 	local getalldata= (gg.makeRequest(url).content)
